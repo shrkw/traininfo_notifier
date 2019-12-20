f98b59a97e9703450b9b02c2b546941b2823dca6
shrkw <shrkwh@gmail.com>
2019-12-20 19:38:12 +0900

    comments only to do

---
+++ main.py
@@ -0,0 +1,9 @@
+def main(opt):
+    # fetch info
+    # synthesize text
+    # send to chromecast (as a promise, audio file is hosted by another http.server process)
+    pass
+
+
+if __name__ == "__main__":
+    main({})
