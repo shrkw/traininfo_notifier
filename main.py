ba14fce046cdb681c77f91a168df0a53160ac861
shrkw <shrkwh@gmail.com>
2020-01-27 01:09:31 +0900

    main process create server process

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import http.server
 import os
+import socket
+import socketserver
+from multiprocessing import Process
 
 from caster import Caster
 from speech_synthesizer import SpeechSynthesizer
@@ -9,6 +13,13 @@ server_host = os.getenv("SERVER_HOST")
 train_info_url = os.getenv("TRAIN_INFO_URL")
 
 
+def serve(port):
+    Handler = http.server.SimpleHTTPRequestHandler
+    with socketserver.TCPServer(("", port), Handler) as httpd:
+        print("serving at port", port)
+        httpd.serve_forever()
+
+
 def main():
     # fetch info
     scraper = YahooTrainInfoScraper()
@@ -16,6 +27,10 @@ def main():
     # synthesize text
     synth = SpeechSynthesizer()
     file_name = synth.synthesize(f"{title}の運行情報です。 {status}")
+
+    p = Process(target=serve, args=(8000,), daemon=True)
+    p.start()
+
     # send to chromecast (as a promise, audio file is hosted by another http.server process)
     caster = Caster(friendly_name)
     caster.cast(f"http://{server_host}/{file_name}")
