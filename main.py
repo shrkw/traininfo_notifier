43bc486eaec90a36ff2ed7ce8ccd63ee0d59c557
shrkw <shrkwh@gmail.com>
2020-01-27 01:22:29 +0900

    get addr from gethostbyname

--- main.py
+++ main.py
@@ -9,18 +9,24 @@ from speech_synthesizer import SpeechSynthesizer
 from yahoo_train_info_scraper import YahooTrainInfoScraper
 
 friendly_name = os.getenv("FRIENDLY_NAME")
-server_host = os.getenv("SERVER_HOST")
 train_info_url = os.getenv("TRAIN_INFO_URL")
 
 
 def serve(port):
+    print("serving at port", port)
     Handler = http.server.SimpleHTTPRequestHandler
     with socketserver.TCPServer(("", port), Handler) as httpd:
-        print("serving at port", port)
         httpd.serve_forever()
 
 
+def ip_addr():
+    host = socket.gethostname()
+    ip = socket.gethostbyname(host)
+    return ip
+
+
 def main():
+    print("Start")
     # fetch info
     scraper = YahooTrainInfoScraper()
     title, status = scraper.fetch_train_info(train_info_url)
@@ -28,12 +34,16 @@ def main():
     synth = SpeechSynthesizer()
     file_name = synth.synthesize(f"{title}の運行情報です。 {status}")
 
-    p = Process(target=serve, args=(8000,), daemon=True)
+    port = 8000
+    p = Process(target=serve, args=(port,), daemon=True)
     p.start()
 
     # send to chromecast (as a promise, audio file is hosted by another http.server process)
     caster = Caster(friendly_name)
-    caster.cast(f"http://{server_host}/{file_name}")
+    server_host = ip_addr()
+    print(f"serve at {server_host}:{port}")
+    caster.cast(f"http://{server_host}:{port}/{file_name}")
+    print("Finish")
 
 
 if __name__ == "__main__":
