5023bdfaffb2ba6a78859a6c90c98a8f2944b6bb
shrkw <shrkwh@gmail.com>
2020-07-01 01:03:15 +0000

    add main for forecast

---
+++ main_forecast.py
@@ -0,0 +1,39 @@
+import datetime
+import os
+from pathlib import Path
+
+from caster import Caster
+from server import Server
+from speech_synthesizer import SpeechSynthesizer
+from yahoo_forecast_api import YahooForecastApi
+from yahoo_train_info_scraper import YahooTrainInfoScraper
+
+friendly_name = os.getenv("FRIENDLY_NAME")
+train_info_url = os.getenv("TRAIN_INFO_URL")
+location = os.getenv("LOCATION")
+yahoo_app_id = os.getenv("YAHOO_APP_ID")
+
+
+def main():
+    print("Start")
+    # fetch info
+    api = YahooForecastApi(location, yahoo_app_id)
+    status = api.run()
+    if status is None:
+        return
+
+    # synthesize text
+    dest_path = Path("tmp/forecast.mp3")
+
+    if not dest_path.exists():
+        synth = SpeechSynthesizer()
+        synth.synthesize(status, dest_path)
+
+    server = Server()
+    server.start(friendly_name, dest_path)
+
+    print("Finish")
+
+
+if __name__ == "__main__":
+    main()
